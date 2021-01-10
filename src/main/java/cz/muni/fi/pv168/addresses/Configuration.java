36c5aa40c61109d279bf325310a43febffc9f375
Petr Adamek <petr.adamek@bilysklep.cz>
2021-01-10 22:07:46 +0100

    Configuration enhanced with user friendly description

--- src/main/java/cz/muni/fi/pv168/addresses/Configuration.java
+++ src/main/java/cz/muni/fi/pv168/addresses/Configuration.java
@@ -14,38 +14,55 @@ import java.util.function.Function;
 
 enum Configuration {
 
-    SIMPLE_FINDER_WITH_FOR_EACH_STRATEGY(dataLoader -> {
-        var searchStrategy = new ForEachSearchStrategy();
-        return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
-    }),
+    SIMPLE_FINDER_WITH_FOR_EACH_STRATEGY(
+            "#1 Sequential (list-based) finder using for-each loop",
+            dataLoader -> {
+                var searchStrategy = new ForEachSearchStrategy();
+                return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
+            }),
 
-    SIMPLE_FINDER_WITH_STREAM_STRATEGY(dataLoader -> {
-        var searchStrategy = new StreamSearchStrategy();
-        return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
-    }),
+    SIMPLE_FINDER_WITH_STREAM_STRATEGY(
+            "#2 Sequential (list-based) finder using streams (single thread)",
+            dataLoader -> {
+                var searchStrategy = new StreamSearchStrategy();
+                return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
+            }),
 
-    SIMPLE_FINDER_WITH_PARALLEL_STREAM_STRATEGY(dataLoader -> {
-        var searchStrategy = new ParallelStreamSearchStrategy();
-        return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
-    }),
+    SIMPLE_FINDER_WITH_PARALLEL_STREAM_STRATEGY(
+            "#3 Sequential (list-based) finder using parallel streams (multiple threads)",
+            dataLoader -> {
+                var searchStrategy = new ParallelStreamSearchStrategy();
+                return new SimpleAddressFinderFactory(dataLoader, searchStrategy);
+            }),
 
-    INDEXED_FINDER_WITH_INDEXED_GROUP(dataLoader -> {
-        var addressGroupFactory = new IndexedAddressGroup.Factory();
-        return new IndexedAddressFinderFactory(dataLoader, addressGroupFactory);
-    }),
+    INDEXED_FINDER_WITH_INDEXED_GROUP(
+            "#4 Indexed (map-based) finder using indexed (map-based) AddressGroup",
+            dataLoader -> {
+                var addressGroupFactory = new IndexedAddressGroup.Factory();
+                return new IndexedAddressFinderFactory(dataLoader, addressGroupFactory);
+            }),
 
-    INDEXED_FINDER_WITH_SIMPLE_GROUP(dataLoader -> {
-        var addressGroupFactory = new SimpleAddressGroup.Factory();
-        return new IndexedAddressFinderFactory(dataLoader, addressGroupFactory);
-    });
+    INDEXED_FINDER_WITH_SIMPLE_GROUP(
+            "#5 Indexed (map-based) finder using sequential (list-based) AddressGroup",
+            dataLoader -> {
+                var addressGroupFactory = new SimpleAddressGroup.Factory();
+                return new IndexedAddressFinderFactory(dataLoader, addressGroupFactory);
+            });
 
     private final Function<DataLoader, AddressFinderFactory> factoryFunction;
+    private final String description;
 
-    Configuration(Function<DataLoader, AddressFinderFactory> factoryFunction) {
+    Configuration(String description, Function<DataLoader, AddressFinderFactory> factoryFunction) {
         this.factoryFunction = factoryFunction;
+        this.description = description;
     }
 
     AddressFinderFactory createAddressFinderFactory(DataLoader dataLoader) {
         return factoryFunction.apply(dataLoader);
+    }
+
+    @Override
+    public String toString() {
+        return description;
     }
 }
