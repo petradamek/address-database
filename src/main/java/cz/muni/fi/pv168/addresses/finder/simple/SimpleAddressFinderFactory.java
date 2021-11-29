b399fd8932c624a7499ab960c7090d0d5cadad11
Petr Adamek <petr.adamek@bilysklep.cz>
2021-11-29 14:42:28 +0100

    Refactoring: DataLoader extended to simplify usage when just List<Address> is needed

--- src/main/java/cz/muni/fi/pv168/addresses/finder/simple/SimpleAddressFinderFactory.java
+++ src/main/java/cz/muni/fi/pv168/addresses/finder/simple/SimpleAddressFinderFactory.java
@@ -1,17 +1,10 @@
 package cz.muni.fi.pv168.addresses.finder.simple;
 
-import cz.muni.fi.pv168.addresses.model.Address;
-import cz.muni.fi.pv168.addresses.model.Address.HouseNoType;
-import cz.muni.fi.pv168.addresses.model.AddressBase;
 import cz.muni.fi.pv168.addresses.finder.AddressFinder;
 import cz.muni.fi.pv168.addresses.finder.AddressFinderFactory;
-import cz.muni.fi.pv168.addresses.model.SimpleAddress;
-import cz.muni.fi.pv168.addresses.loader.AddressHandler;
 import cz.muni.fi.pv168.addresses.loader.DataLoader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Factory for {@link SimpleAddressFinder}.
@@ -27,19 +20,6 @@ public final class SimpleAddressFinderFactory extends AddressFinderFactory {
 
     @Override
     public AddressFinder newAddressFinder() throws IOException {
-        AddressHandlerImpl addressHandler = new AddressHandlerImpl();
-        getDataLoader().loadData(addressHandler);
-        return new SimpleAddressFinder(addressHandler.addresses, searchStrategy);
-    }
-
-    private static class AddressHandlerImpl implements AddressHandler {
-
-        private final List<Address> addresses = new ArrayList<>();
-
-        @Override
-        public void processAddress(AddressBase addressBase, String orientationNo, Integer houseNo, HouseNoType houseNoType) {
-            Address address = new SimpleAddress(addressBase, orientationNo, houseNo, houseNoType);
-            addresses.add(address);
-        }
+        return new SimpleAddressFinder(getDataLoader().loadData(), searchStrategy);
     }
 }
--- src/main/java/cz/muni/fi/pv168/addresses/loader/DataLoader.java
+++ src/main/java/cz/muni/fi/pv168/addresses/loader/DataLoader.java
@@ -1,6 +1,12 @@
 package cz.muni.fi.pv168.addresses.loader;
 
+import cz.muni.fi.pv168.addresses.model.Address;
+import cz.muni.fi.pv168.addresses.model.SimpleAddress;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * This is common interface for data loading service.
@@ -10,4 +16,15 @@ import java.io.IOException;
 public interface DataLoader {
 
     void loadData(AddressHandler addressHandler) throws IOException;
+
+    default void loadData(Consumer<Address> addressConsumer) throws IOException {
+        loadData((addressBase, orientationNo, houseNo, houseNoType) -> addressConsumer.accept(
+                new SimpleAddress(addressBase, orientationNo, houseNo, houseNoType)));
+    }
+
+    default List<Address> loadData() throws IOException {
+        List<Address> addresses = new ArrayList<>();
+        loadData(addresses::add);
+        return addresses;
+    }
 }
